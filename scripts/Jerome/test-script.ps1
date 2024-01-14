5ac210f64af9fe32c63e2711de2f0fd62f9b63d8
Jérôme Bezet-Torres [MVP] <JM2K69@users.noreply.github.com>
2024-01-14 16:01:12 +0100

    Add files via upload

---
+++ scripts/Jerome/test-script.ps1
@@ -0,0 +1,45 @@
+<#PSScriptInfo
+.VERSION 23.7.16.1
+.GUID c7c5121e-21b1-4594-82fa-035911a32584
+.AUTHOR Bezet-Torres Jérôme
+.COMPANYNAME Bezet-Torres Jérôme
+.COPYRIGHT (c) 2023 Bezet-Torres Jérôme. All rights reserved.
+.TAGS DEV PARAM
+.LICENSEURI 
+.PROJECTURI https://github.com/OSDeploy/PwshHub
+.ICONURI 
+.EXTERNALMODULEDEPENDENCIES 
+.REQUIREDSCRIPTS 
+.EXTERNALSCRIPTDEPENDENCIES 
+.RELEASENOTES
+#>
+#Requires -PSEdition Desktop
+#Requires -Version 5.1
+<#
+.DESCRIPTION
+Some Defaults
+#>
+[CmdletBinding()]
+param(
+
+  [Parameter(Mandatory=$true, 
+  ValueFromPipeline=$true,
+  ValueFromPipelineByPropertyName=$true, 
+  ValueFromRemainingArguments=$false, 
+  Position=0)]
+[ValidateNotNull()]
+[ValidateNotNullOrEmpty()]
+[ValidateCount(0,5)]
+[Alias("p1")] 
+$Testvar,
+
+[Parameter()]
+[AllowNull()]
+[ValidateSet('Startup', 'Shutdown', 'LogOn', 'LogOff')]
+[String]$Testvar2
+
+
+
+)
+
+Write-host -Object "Hello World $Testvar and $Testvar2"
---
+++ scripts/README.md
@@ -0,0 +1,8 @@
+OSDCloud Scripts is really really really unfinished
+Please don't count on these scripts working 100%
+
+Currently this is used for testing and development purposes only
+
+Script: Select a script from the combobox
+Name: Scripts will be limited by a value match in the script name and relative path
+Search: Scripts will be limited by a value match in the script content
